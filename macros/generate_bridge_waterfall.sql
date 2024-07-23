3e472cec07eeacabedb4c19d1a0728feb4e59de8
craiglathrop <clathrop@coldborecapital.com>
2024-07-23 09:28:49 -0700

    Parking

--- macros/generate_bridge_waterfall.sql
+++ macros/generate_bridge_waterfall.sql
@@ -1,38 +1,92 @@
-{%- macro bridge_waterfall(period, date_field, dimension, metric, model=None, cte=None, start_date=None) -%}
+{%- macro bridge_waterfall(period, date_field, dimension, metric, model=None, cte=None, filter_dimensions=None, start_date=None) -%}
 
-{%- if period == 'day' or period == 'week' -%}
-{%- set prefix = 'MM-DD' -%}
-{%- elif period == 'month' -%}
-{%- set prefix = 'YYYY-MM' -%}
-{%- elif period == 'quarter' -%}
-{%- set prefix = 'YYYY' -%} {# The quarter is handled differently than the other periods. #}
-{%- elif period == 'year' -%}
-{%- set prefix = 'YYYY' -%}
-{%- else -%}
-{%- set prefix = 'ERROR' -%}
+{%- if target.type == 'bigquery' -%}
+    {% if period == 'day' or period == 'week' %}
+        {% set prefix = '%m-%d' %}
+    {% elif period == 'month' %}
+        {% set prefix = '%Y-%m' %}
+    {% elif period == 'quarter' %}
+        {% set prefix = '%Y' %}
+    {% elif period == 'year' %}
+        {% set prefix = '%Y' %}
+    {% else %}
+        {% set prefix = 'ERROR' %}
+    {% endif %}
+{% else %}
+    {% if period == 'day' or period == 'week' %}
+        {% set prefix = 'MM-DD' %}
+    {% elif period == 'month' %}
+        {% set prefix = 'YYYY-MM' %}
+     {% elif period == 'quarter' %}
+        {% set prefix = 'YYYY' %} {# The quarter is handled differently than the other periods. #}
+     {% elif period == 'year' %}
+        {% set prefix = 'YYYY' %}
+     {% else %}
+        {% set prefix = 'ERROR' %}
+    {% endif %}
 {%- endif -%}
+
+{%- if target.type == 'bigquery' -%}
+    {% set quarter_date_part = "extract(quarter from waterfall_date_at)" %}
+    {% set date_prefix = "format_timestamp('" + prefix +"', waterfall_date_at)" %}
+    {% set full_date_char_rc = "format_timestamp('%Y-%m', rc.waterfall_date_at)" %}
+    {% set full_date_char = "format_timestamp('%Y-%m', waterfall_date_at)" %}
+{%- else %}
+    {# This should cover snowflake, redshift, and postgres #}
+    {% set quarter_date_part = "date_part('quarter', waterfall_date_at)" %}
+    {% set date_prefix = "to_char(waterfall_date_at, '" + prefix +"')" %}
+    {% set full_date_char_rc = "to_char(rc.waterfall_date_at, 'YYYY-MM-DD')" %}
+    {% set full_date_char = "to_char(waterfall_date_at, 'YYYY-MM-DD')" %}
+{%- endif -%}
+
 
 {%- if model -%}with {% endif %} {{ metric }}_by_{{ period }}_bu as (
     select
-        date_trunc('{{ period }}', {{ date_field }}) as waterfall_date_at
+          {{ dbt.date_trunc(period, date_field) }} as waterfall_date_at
          , {{ dimension }}
+            {%- if filter_dimensions %}
+            {%- for filter_dim in filter_dimensions %}
+         , {{ filter_dim }}
+            {%- endfor %}
+            {%- endif %}
          , sum({{ metric }})  as {{ metric }}
     from {% if model -%}{{ ref(model) }}{% else %}{{ cte }}{% endif %}
     {%- if start_date is not none %}
-    where date({{ date_field }}) between '{{ start_date }}' and dateadd('days', -1, date_trunc('{{ period }}', current_date))
+    where date({{ date_field }}) between '{{ start_date }}' and {{ dbt.dateadd(datepart='second',
+                                                                               interval=-1,
+                                                                               from_date_or_timestamp=dbt.date_trunc(period, dbt.current_timestamp())) }}
     {%- else %}
-    where date({{ date_field }}) < dateadd('days', -1, date_trunc('{{ period }}', current_date))
+    where date({{ date_field }}) < {{ dbt.dateadd(datepart='second',
+                                                  interval=-1,
+                                                  from_date_or_timestamp=dbt.date_trunc(period, dbt.current_timestamp())) }}
     {% endif %}
-group by 1, 2
+group by waterfall_date_at
+    , {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for dimension in filter_dimensions %}
+    , {{ dimension }}
+    {%- endfor %}
+    {%- endif %}
     )
 
 , {{ metric }}_changes as (
 select
     waterfall_date_at
         , {{ dimension }}
+        {%- if filter_dimensions %}
+        {%- for filter_dim in filter_dimensions %}
+            , {{ filter_dim }}
+        {%- endfor %}
+        {%- endif %}
         , {{ metric }}                                                         as current_period_{{ metric }}
         , lag({{ metric }})
-           over (partition by {{ dimension }} order by waterfall_date_at desc) as previous_period_{{ metric }}
+           over (partition by {{ dimension }}
+                            {%- if filter_dimensions %}
+                            {%- for filter_dim in filter_dimensions %}
+                                , {{ filter_dim }}
+                            {%- endfor %}
+                            {%- endif %}
+                 order by waterfall_date_at desc) as previous_period_{{ metric }}
         , previous_period_{{ metric }} - {{ metric }}                          as {{ metric }}_change
 from {{ metric }}_by_{{ period }}_bu
     )
@@ -40,9 +94,19 @@ from {{ metric }}_by_{{ period }}_bu
 , {{ period }}_totals as (
 select
     waterfall_date_at
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+        , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
     , sum({{ metric }}) as total_{{ metric }}
 from {{ metric }}_by_{{ period }}_bu
-group by 1
+group by waterfall_date_at
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+        , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
     )
 -- These prefixes
  , hidden_prefixes as (
@@ -115,53 +179,98 @@ select 53 as period , '\u202F\u202F\u202F\u202F\u202D' as hidden_value
 
 , waterfall_data as (
 select
-    date_trunc('{{ period }}', rc.waterfall_date_at) as waterfall_date_at
-    , to_char(rc.waterfall_date_at, 'YYYY-MM-DD')
+    {{ dbt.date_trunc(period, 'rc.waterfall_date_at') }}          as waterfall_date_at
+    -- Important, no filter dimensions in the sort order value. That will break the script when run as it will create
+    -- unique rows.
+    , {{ full_date_char_rc }}
     || '-'
-    || lpad((row_number() over (order by rc.waterfall_date_at, rc.{{ dimension }}) + 1)::varchar, 3, '0') as sort_order
-    , hp.hidden_value || rc.{{ dimension }}                                    as label
+    || lpad((rank() over
+        (partition by rc.{{ dimension }}
+        order by rc.waterfall_date_at, rc.{{ dimension }}) + 1)::varchar, 3, '0')    as sort_order
+    , hp.hidden_value || rc.{{ dimension }}                       as label
+    -- , rc.{{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+        , rc.{{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
     , coalesce(sum(rc.{{ metric }}_change)
-        over (partition by rc.waterfall_date_at order by rc.{{ dimension }} rows between unbounded preceding and 1 preceding)
-        , 0
-    ) + mt.total_{{ metric }}                                                                             as bar_start
-    , false                                                                                               as is_total
-    , rc.{{ metric }}_change                                                                              as value
+        over (partition by rc.waterfall_date_at
+                        {%- if filter_dimensions %}
+                        {%- for filter_dim in filter_dimensions %}
+                            , rc.{{ filter_dim }}
+                        {%- endfor %}
+                        {%- endif %}
+              order by rc.{{ dimension }}
+              rows between unbounded preceding and 1 preceding)
+        , 0) + mt.total_{{ metric }}                               as bar_start
+    , false                                                        as is_total
+    , rc.{{ metric }}_change                                       as value
 from {{ metric }}_changes as rc
+    -- Join in the month level totals.
     left join {{ period }}_totals as mt on rc.waterfall_date_at = mt.waterfall_date_at
+        {%- if filter_dimensions %}
+        {%- for filter_dim in filter_dimensions %}
+        and rc.{{ filter_dim }} = mt.{{ filter_dim }}
+        {%- endfor %}
+        {%- endif %}
     -- Join to the hidden chars based on the period value. This will evaluate to something like
     -- month(rc.waterfall_date_at) = hp.period
     left join hidden_prefixes as hp on {{ period }}(rc.waterfall_date_at) = hp.period
 -- This filter is configured so that the total for the final {{ period }} will be in the data, but the period change components will not.
-where rc.waterfall_date_at < date_trunc('{{ period }}', dateadd('days', -1, date_trunc('{{ period }}', current_date)))
+where rc.waterfall_date_at < {{ dbt.date_trunc(period, dbt.dateadd('month', -1, dbt.current_timestamp())) }}
+
+    -- date_trunc('{{ period }}', dateadd('days', -1, date_trunc('{{ period }}', dbt.current_timestamp())))
 
 union all
 
 select
     waterfall_date_at
-    , to_char(waterfall_date_at, 'YYYY-MM-DD') || '-' || '001' as sort_order
-    {%- if period == 'quarter' -%}
-    , to_char(waterfall_date_at, '{{ prefix }}') || ' Q' || quarter(waterfall_date_at)               as label
-    {%- else %}
-    , to_char(waterfall_date_at, '{{ prefix }}')               as label
+    , {{ full_date_char }} || '-' || '001'                     as sort_order
+        {%- if period == 'quarter' -%}
+        , {{ date_prefix }} || ' Q' || {{ quarter_date_part }} as label
+        {%- else %}
+        , {{ date_prefix }}                                    as label
+        {%- endif %}
+    --, {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for dimension in filter_dimensions %}
+        , {{ dimension }}
+    {%- endfor %}
     {%- endif %}
     , 0                                                        as bar_start
     , true                                                     as is_total
     , sum(current_period_{{ metric }})                         as value
 from {{ metric }}_changes
-group by all
+group by waterfall_date_at
+        , sort_order
+        , label
+        --, {{ dimension }}
+        {%- if filter_dimensions %}
+        {%- for filter_dim in filter_dimensions %}
+        , {{ filter_dim }}
+        {%- endfor %}
+        {%- endif %}
+        , bar_start
+        , is_total
 
     )
-
+, final as (
 -- Create the total bar components
 select
     waterfall_date_at
-     , sort_order
-     , label
-     , is_total
-     , 'Total' as bar_type
-     , case
-       when is_total then value
-       else null
+    , sort_order
+    , label
+    -- , {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+    , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
+    , is_total
+    , 'Total' as bar_type
+    , case
+        when is_total then value
     end       as value
 from waterfall_data
 
@@ -170,14 +279,20 @@ union all
 -- Create the hidden "white" bar components
 select
     waterfall_date_at
-     , sort_order
-     , label
-     , is_total
-     , 'Hidden' as bar_type
-     , case when is_total then null
-       when value < 0 then bar_start + value -- value is negative here, so reduce height
-       else bar_start
-    end         as value
+    , sort_order
+    , label
+    -- , {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+    , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
+    , is_total
+    , 'Hidden' as bar_type
+    , case when is_total then null
+        when value < 0 then bar_start + value -- value is negative here, so reduce height
+        else bar_start
+    end as value
 from waterfall_data
 
 union all
@@ -185,15 +300,21 @@ union all
 -- Gain bars
 select
     waterfall_date_at
-     , sort_order
-     , label
-     , is_total
-     , 'Gain' as bar_type
-     , case
-       when is_total then null
-       when value < 0 then null
-       else value
-    end       as value
+    , sort_order
+    , label
+    -- , {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+    , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
+    , is_total
+    , 'Gain' as bar_type
+    , case
+        when is_total then null
+        when value < 0 then null
+        else value
+    end as value
 from waterfall_data
 
 union all
@@ -201,18 +322,42 @@ union all
 -- Loss bars
 select
     waterfall_date_at
-     , sort_order
-     , label
-     , is_total
-     , 'Loss' as bar_type
-     , case
-       when is_total then null
-       when value > 0 then null
+    , sort_order
+    , label
+    -- , {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+    , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
+    , is_total
+    , 'Loss' as bar_type
+    , case
+    when is_total then null
+    when value > 0 then null
     -- Set the bar value to positive so it will stack right.
-       else -value
-    end       as value
+    else - value
+    end as value
 from waterfall_data
-where is_total = false
+
+    )
+
+select
+    waterfall_date_at
+    , sort_order
+    , label
+    -- , {{ dimension }}
+    {%- if filter_dimensions %}
+    {%- for filter_dim in filter_dimensions %}
+    , {{ filter_dim }}
+    {%- endfor %}
+    {%- endif %}
+    , is_total
+    , bar_type
+    , value
+
+from final
+where value is not null
 order by sort_order, bar_type desc
 
 {%- endmacro -%}
